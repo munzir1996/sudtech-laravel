068310f08d645dd8088a2c044f60d3172e38d7f6
Munzir1996 <munzirmukhtar996@gmail.com>
2020-09-01 21:23:43 +0200

    Dump solution

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,11 @@
       <title></title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('js/app.js') }}" defer></script>
-    @routes
-  </head>
+    <link href="{{ mix('resources/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('resources/js/app.js') }}" defer></script>
+</head>
   <body class="bg-gray-200 min-h-screen font-base">
     @inertia
+    @routes
   </body>
 </html>
